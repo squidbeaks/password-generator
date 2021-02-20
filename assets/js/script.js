4a6252a3d066285bebe71126cb1e5589818dde4e
Leah Russell <leahsigridrussell@gmail.com>
2021-02-20 15:54:36 -0800

    Cleaning up my code and console.logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,21 @@ var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numericChar = "0123456789"; 
 var specialCharacter = "!#$%&'()*+-/<=>?@[]^_`{|}~";
-var lengthPrompt;
+var passwordLength;
 
 var charSet;
 
-var passwordLength = function() {
-  lengthPrompt = prompt(
-    // prompt for password length (8-128)
+var lengthPrompt = function() {
+  passwordLength = prompt(
     "How long would you like your password? It must be between 8 and 128 characters."
     );
-    // need to validate there is proper input
-    if (lengthPrompt < 8 || lengthPrompt > 128) {
+
+    if (passwordLength < 8 || passwordLength > 128) {
       alert("Password should be between 8 and 128 characters");
       return generatePassword();
     }
 
-    if (lengthPrompt === "" || lengthPrompt === null || isNaN(lengthPrompt)) {
+    if (passwordLength === "" || passwordLength === null || isNaN(passwordLength)) {
       alert("Your entry is invalid");
       return generatePassword();
     }
@@ -43,7 +42,8 @@ var charTypePrompt = function() {
   
       if (specCharPrompt) {
         charSet = charSet + specialCharacter;
-      }  
+      }
+
       if(upperCasePrompt || lowerCasePrompt || numCharPrompt || specCharPrompt) {
         alert('Your password is being generated!'); 
       }
@@ -51,36 +51,29 @@ var charTypePrompt = function() {
         alert("You need to select at least one character type.");
         charTypePrompt();
       }
-      console.log("charSet: " + charSet);
 };
 
-var getRandomIndex = function(str) {
-  return Math.floor(Math.random() * str);
+var getRandomIndex = function(stringLength) {
+  return Math.floor(Math.random() * stringLength);
 }
 
 var generatePassword = function() {
   var charIndex;
-  passwordLength();
-  console.log(lengthPrompt);
+  lengthPrompt();
   charTypePrompt();
-  var newPassword = getRandomIndex(charSet.length);
+  var newPassword = '';
 
 
-  for (var i = 0; i < lengthPrompt; i++) {
+  for (var i = 0; i < passwordLength; i++) {
     charIndex = getRandomIndex(charSet.length)
-    console.log("Charset length: " + charSet.length);
-    console.log("CharIndex: " + charIndex);
     newPassword += charSet.charAt(charIndex);
-    console.log(newPassword);
   }
 
   return newPassword;
 };
 
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -88,21 +81,4 @@ function writePassword() {
   passwordText.value = password;
 };
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
